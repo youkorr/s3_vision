9c28e6f5b8db6a1e7800f56ca9cfeaa9f99473a0
youkorr <96953496+youkorr@users.noreply.github.com>
2026-05-06 01:12:01 +0200

    Add files via upload

---
+++ components/models/coco_detect/coco_detect.cpp
@@ -0,0 +1,93 @@
+#include "coco_detect.hpp"
+#include "esp_log.h"
+#include <filesystem>
+
+#if CONFIG_COCO_DETECT_MODEL_IN_FLASH_RODATA
+extern const uint8_t coco_detect_espdl[] asm("_binary_coco_detect_espdl_start");
+static const char *path = (const char *)coco_detect_espdl;
+#elif CONFIG_COCO_DETECT_MODEL_IN_FLASH_PARTITION
+static const char *path = "coco_det";
+#else
+#if !defined(CONFIG_BSP_SD_MOUNT_POINT)
+#define CONFIG_BSP_SD_MOUNT_POINT "/sdcard"
+#endif
+#endif
+namespace coco_detect {
+Yolo11n::Yolo11n(const char *model_name, float score_thr, float nms_thr)
+{
+#if !CONFIG_COCO_DETECT_MODEL_IN_SDCARD
+    bool param_copy = true;
+    if (heap_caps_get_total_size(MALLOC_CAP_SPIRAM) < 1024 * 1024 * 9) {
+        param_copy = false;
+    }
+    m_model = new dl::Model(path,
+                            model_name,
+                            static_cast<fbs::model_location_type_t>(CONFIG_COCO_DETECT_MODEL_LOCATION),
+                            0,
+                            dl::MEMORY_MANAGER_GREEDY,
+                            nullptr,
+                            param_copy);
+#else
+    auto sd_path = std::filesystem::path(CONFIG_BSP_SD_MOUNT_POINT) / CONFIG_COCO_DETECT_MODEL_SDCARD_DIR / model_name;
+    m_model = new dl::Model(sd_path.c_str(), fbs::MODEL_LOCATION_IN_SDCARD);
+#endif
+    m_model->minimize();
+    m_image_preprocessor = new dl::image::ImagePreprocessor(m_model, {0, 0, 0}, {255, 255, 255});
+    m_image_preprocessor->enable_letterbox({114, 114, 114});
+    m_postprocessor = new dl::detect::yolo11PostProcessor(
+        m_model, m_image_preprocessor, 0.25, 0.7, 10, {{8, 8, 4, 4}, {16, 16, 8, 8}, {32, 32, 16, 16}});
+}
+
+} // namespace coco_detect
+
+COCODetect::COCODetect(model_type_t model_type, bool lazy_load) : m_model_type(model_type)
+{
+    switch (model_type) {
+    case model_type_t::YOLO11N_S8_V1:
+    case model_type_t::YOLO11N_S8_V2:
+    case model_type_t::YOLO11N_S8_V3:
+    case model_type_t::YOLO11N_320_S8_V3:
+        m_score_thr[0] = coco_detect::Yolo11n::default_score_thr;
+        m_nms_thr[0] = coco_detect::Yolo11n::default_nms_thr;
+        break;
+    }
+    if (lazy_load) {
+        m_model = nullptr;
+    } else {
+        load_model();
+    }
+}
+
+void COCODetect::load_model()
+{
+    switch (m_model_type) {
+    case model_type_t::YOLO11N_S8_V1:
+#if CONFIG_FLASH_COCO_DETECT_YOLO11N_S8_V1 || CONFIG_COCO_DETECT_MODEL_IN_SDCARD
+        m_model = new coco_detect::Yolo11n("coco_detect_yolo11n_s8_v1.espdl", m_score_thr[0], m_nms_thr[0]);
+#else
+        ESP_LOGE("coco_detect", "coco_detect_yolo11n_s8_v1 is not selected in menuconfig.");
+#endif
+        break;
+    case model_type_t::YOLO11N_S8_V2:
+#if CONFIG_FLASH_COCO_DETECT_YOLO11N_S8_V2 || CONFIG_COCO_DETECT_MODEL_IN_SDCARD
+        m_model = new coco_detect::Yolo11n("coco_detect_yolo11n_s8_v2.espdl", m_score_thr[0], m_nms_thr[0]);
+#else
+        ESP_LOGE("coco_detect", "coco_detect_yolo11n_s8_v2 is not selected in menuconfig.");
+#endif
+        break;
+    case model_type_t::YOLO11N_S8_V3:
+#if CONFIG_FLASH_COCO_DETECT_YOLO11N_S8_V3 || CONFIG_COCO_DETECT_MODEL_IN_SDCARD
+        m_model = new coco_detect::Yolo11n("coco_detect_yolo11n_s8_v3.espdl", m_score_thr[0], m_nms_thr[0]);
+#else
+        ESP_LOGE("coco_detect", "coco_detect_yolo11n_s8_v3 is not selected in menuconfig.");
+#endif
+        break;
+    case model_type_t::YOLO11N_320_S8_V3:
+#if CONFIG_FLASH_COCO_DETECT_YOLO11N_320_S8_V3 || CONFIG_COCO_DETECT_MODEL_IN_SDCARD
+        m_model = new coco_detect::Yolo11n("coco_detect_yolo11n_320_s8_v3.espdl", m_score_thr[0], m_nms_thr[0]);
+#else
+        ESP_LOGE("coco_detect", "coco_detect_yolo11n_320_s8_v3 is not selected in menuconfig.");
+#endif
+        break;
+    }
+}
---
+++ components/models/coco_detect/coco_detect.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "dl_detect_base.hpp"
+#include "dl_detect_yolo11_postprocessor.hpp"
+
+namespace coco_detect {
+class Yolo11n : public dl::detect::DetectImpl {
+public:
+    static constexpr float default_score_thr = 0.25;
+    static constexpr float default_nms_thr = 0.7;
+    Yolo11n(const char *model_name, float score_thr, float nms_thr);
+};
+} // namespace coco_detect
+
+class COCODetect : public dl::detect::DetectWrapper {
+public:
+    typedef enum {
+        YOLO11N_S8_V1,
+        YOLO11N_S8_V2,
+        YOLO11N_S8_V3,
+        YOLO11N_320_S8_V3,
+    } model_type_t;
+    COCODetect(model_type_t model_type = static_cast<model_type_t>(CONFIG_DEFAULT_COCO_DETECT_MODEL),
+               bool lazy_load = true);
+
+private:
+    void load_model() override;
+
+    model_type_t m_model_type;
+};
